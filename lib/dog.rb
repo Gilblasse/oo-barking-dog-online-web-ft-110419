302b464f9624628626e0af8a3a914449354a04cd
Nethelbert <gilblasse@gmail.com>
2019-11-09 04:49:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,15 @@
 class Dog 
-  attr_accessor :name,:bark
-  def initialize(name,bark)
+  attr_accessor :name
+  attr_reader :bark
+  
+  def initialize(name)
+    @name = name
   end
+  
+  def bark
+    puts "Woof!"
+  end
+  
 end
 fido = Dog.new
 fido.name = "Fido"
